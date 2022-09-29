39fcd2263b18ace3aa93e1350b90b50e5f4385a2
Андрій Шевельов <ashevelevmti@gmail.com>
2022-09-29 23:43:46 +0300

    fix yarn

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,9 @@
 /** @format */
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  //added this
-  resolver: {
-    sourceExts: ["jsx", "js", "ts", "tsx", "cjs"],
-  },
-};
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push("cjs");
+
+module.exports = defaultConfig;
